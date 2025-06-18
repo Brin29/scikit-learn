4a9493183ff0f944ae27f14c33237b548b275b30
Brin29 <breinerstivenparracortes50@gmail.com>
2025-06-18 15:39:12 -0500

    cambios

--- gradient_boosting.py
+++ gradient_boosting.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import BaggingClassifier
+
+from sklearn.ensemble import GradientBoostingClassifier
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -15,13 +15,7 @@ if __name__ == "__main__":
 
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35)
 
-  knn_class = KNeighborsClassifier().fit(X_train, y_train)
-  knn_pred = knn_class.predict(X_test)
-
-  print('='*64)
-  print(accuracy_score(knn_pred, y_test))
-
-  bag_class = BaggingClassifier(estimator=KNeighborsClassifier(), n_estimators=50).fit(X_train, y_train)
-  bag_pred = bag_class.predict(X_test)
-  print("="*64)
-  print(accuracy_score(bag_pred, y_test))
+  boost = GradientBoostingClassifier(n_estimators=50).fit(X_train, y_train)
+  boost_pred = boost.predict(X_test)
+  print("*"*64)
+  print(accuracy_score(boost_pred, y_test))
